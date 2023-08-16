7a69b302fa429403373aed4d0953bdbb3dbffe73
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-08-16 21:47:54 +0300

    Fix linter errors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,5 @@
 class GreetingsController < ApplicationController
-  before_action :set_greeting, only: %i[ show edit update destroy ]
+  before_action :set_greeting, only: %i[show edit update destroy]
 
   # GET /greetings or /greetings.json
   def index
@@ -64,13 +64,14 @@ class GreetingsController < ApplicationController
   # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_greeting
-      @greeting = Greeting.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def greeting_params
-      params.require(:greeting).permit(:message)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_greeting
+    @greeting = Greeting.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def greeting_params
+    params.require(:greeting).permit(:message)
+  end
 end
